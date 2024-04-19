c4c3937305c028bde6ea157f2be4ec5892a1dc18
arfoysal <arfoysal.me@gmail.com>
2024-04-19 22:27:41 +0600

    Add testNG Annotation, Interacting with Web element  and Alert

--- src/test/java/org/example/BrowserSetup.java
+++ src/test/java/org/example/BrowserSetup.java
@@ -1,6 +1,8 @@
 package org.example;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
@@ -16,4 +18,22 @@ public class BrowserSetup {
     public void quitBrowser(){
         browser.quit();
     }
+
+    public WebElement getElement(By locator){
+        return browser.findElement(locator);
+    }
+
+    public void writeOnElement(By locator, String text ){
+        getElement(locator).clear();
+        getElement(locator).sendKeys(text);
+    }
+
+    public Boolean displayStatus(By locator){
+       return getElement(locator).isDisplayed();
+    }
+
+    public void clickOnElemt(By locator){
+        getElement(locator).click();
+    }
 }
+
